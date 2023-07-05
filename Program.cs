0465a2bd22d8bf32a44ebe9fae3430eba5f05e01
Sebastian Chaves <sebastian.chaves1408@gmail.com>
2023-07-05 01:15:30 -0600

    Tipos de variables en C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Boolean | 1 bit | true or false
+bool active = true;
+
+//Byte | 8 bit | 0 to 255
+byte angles = 100;
+
+//Character | 16 bit | all characters
+char initial = '#';
+
+//Short | 16 bit | -32,768 to 32,767
+short lenght = 20000;
+
+//Integer/Entero | 32 bit | -2,147,483,648 to 2,127,483,647
+int year = 2023;
+
+//Float | 32 bit | ±1.5e−45 to ±3.4e38, 7 digits precision
+float microDistance = 0.55555F;
+
+//Double | 64 bit | ±5.0e−324 to ±1.7e308, 15-16 digits precision
+double size = 9999999999.99D;
+
+//Long | 64 bit | -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+long distance = 500000000000;
+
+//Decimal | 128 bit | ±1.0 x 10^-28 to ±7.9 x 10^28, 28-29 digits precision
+decimal measurement = 1.35435634654654654654M;
+
+//String(Texto) | dynamic size
+string name = "Kai"; 
+name = "Sebastian"; //Reasignación
+
+//Ejemplo llamar variable "Console.WriteLine(name);"
